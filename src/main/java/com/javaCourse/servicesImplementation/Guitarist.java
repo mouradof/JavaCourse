7478ba035a83d711ac0e7b5be35d94a1e3b6ef6d
Mourad HADJRES <mouradhadjresp@gmail.com>
2023-01-24 15:55:19 +0100

    Autowire on attribute

--- src/main/java/com/javaCourse/servicesImplementation/Guitarist.java
+++ src/main/java/com/javaCourse/servicesImplementation/Guitarist.java
@@ -2,14 +2,13 @@ package com.javaCourse.servicesImplementation;
 
 import com.javaCourse.services.Musician;
 import com.javaCourse.services.PrepareService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Guitarist implements Musician {
-    private PrepareService prepareService;
-    @Autowired
-    public void setPrepareService(PrepareService prepareService) {
+    private final PrepareService prepareService;
+    public Guitarist(@Qualifier("zenPreparationService") PrepareService prepareService) {
         this.prepareService = prepareService;
     }
     @Override
